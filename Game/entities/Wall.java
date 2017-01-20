4f21ef8d765acb24f688929877161d2fe41479c2
Hamza <hamza@MacBook-Pro-de-Hamza.local>
2017-01-20 22:14:41 +0100

    sprite problem

---
+++ Game/entities/Wall.java
@@ -0,0 +1,32 @@
+package Game.entities;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+
+import gameframework.core.Drawable;
+import gameframework.core.DrawableImage;
+import gameframework.core.GameEntity;
+import gameframework.moves_rules.MoveBlocker;
+
+public class Wall implements  Drawable,MoveBlocker,GameEntity {
+	protected static DrawableImage image =null;
+	int x,y ; 
+	public static final int RENDERING_SIZE=20;
+	 public Wall(Canvas canvas, int x ,int y) {
+		 image=new DrawableImage("images/Wall.gif", canvas);
+		 this.x=x;
+		 this.y=y;
+	}
+	@Override
+	public Rectangle getBoundingBox() {
+		return (new Rectangle(x, y, RENDERING_SIZE, RENDERING_SIZE));
+	}
+
+	@Override
+	public void draw(Graphics g) {
+		g.drawImage(image.getImage(), x, y, RENDERING_SIZE, RENDERING_SIZE,
+				null);
+		
+	}
+}
